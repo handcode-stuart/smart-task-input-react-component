b7bfe85c782c5f9fbfd8ac2533e07b93719968ea
Stuart Morris <admin@handcode.co.uk>
2019-05-03 16:04:45 +0100

    Added years

--- src/components/SmartTaskInput.js
+++ src/components/SmartTaskInput.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { days, months } from "../validation/dates";
+import { days, months, years } from "../validation/dates";
 
 class SmartTaskInput extends Component {
     constructor(props) {
@@ -27,31 +27,32 @@ class SmartTaskInput extends Component {
     onChange = e => {
         e.preventDefault();
         const { value: task } = e.target;
-
         const taskArray = task.split(" ");
 
-        const foundMonths = this.wasDateInputted(taskArray, months);
-        this.setTaskDate(foundMonths);
-
-        const foundDays = this.wasDateInputted(taskArray, days);
-        this.setTaskDay(foundDays);
-
-        this.setTaskToState(task);
+        this.setState(prevState => ({
+            date: {
+                ...prevState.date,
+                day: this.wasDateInputted(taskArray, days),
+                month: this.wasDateInputted(taskArray, months),
+                year: this.wasDateInputted(taskArray, years),
+            },
+            task,
+        }));
     };
 
-    setTaskToState = task => this.setState({ task });
-
-    setTaskDate = month => this.setState(prevState => ({ date: { ...prevState.date, month } }));
-
-    setTaskDay = day => this.setState(prevState => ({ date: { ...prevState.date, day } }));
+    setTaskDate = (inputtedDate, type) => {
+        this.setState(prevState => ({ date: { ...prevState.date, [type]: inputtedDate } }));
+    };
 
     render() {
-        const { month, day } = this.state.date;
+        const { month, day, year } = this.state.date;
+        const { task } = this.state;
         return (
             <div>
                 <input type='text' value={this.state.task} onChange={this.onChange} />
+                <p>Your task is: {task}</p>
                 <p>
-                    {day} {month}
+                    Due: {day} {month} {year}
                 </p>
             </div>
         );
